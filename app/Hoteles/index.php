1dd821ee9e2598a668f97f88de5eb3b0932b6608
DiegoR30 <die-35_45rhotmail.com>
2024-01-19 01:02:39 -0500

    first commit

---
+++ app/Hoteles/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Express reserves</title>
+    <link href="../../assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../assets/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../assets/css/styles.css">
+</head>
+<body class="body">
+
+    <nav class="nav">
+        <header>
+            <h2>Express reserves</h2>
+        </header>
+        <img class="nav--img" src="../../assets/img/logo-hotel.png" alt="">
+    </nav>
+
+    <div class="hero" >
+        <div class="cartaa">
+            <form action="">
+                <header class="title">
+                    <h1 class="text-title">BIENVENIDO</h1>
+                </header>
+                <div class="login">
+                    <input class="input--form" type="text" placeholder="Usuario" require>
+                </div>
+                <div class="login">
+                    <input class="input--form" type="text" placeholder="Contraseña" require>
+                </div>
+                <button class="button" type="submit">Ingresar</button>
+                <div class="item-register">
+                    <p>¿Aun no tienes cuenta? <a href="#">Registrate</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+    
+
+
+<script  src="../../assets/js/bootstrap.bundle.min.js"  ></script>
+</body>
+</html>
